f5daa27c365facd3a85991e377cfdfb669c01e3a
Luiz Ribeiro <luizribeiro@gmail.com>
2020-12-26 13:46:13 -0600

    Cleaned up sensor setup based on bsec example

--- CE_BSEC.h
+++ CE_BSEC.h
@@ -1,10 +1,6 @@
 #include "esphome.h"
 #include "bsec.h"
 
-const uint8_t bsec_config_iaq[] = {
-    #include "config/generic_33v_3s_28d/bsec_iaq.txt"
-};
-
 class CE_BSEC : public PollingComponent, public Sensor {
  public:
   // constructor
@@ -20,17 +16,19 @@ class CE_BSEC : public PollingComponent, public Sensor {
   void setup() override {
     // This will be called by App.setup()
     iaqSensor.begin(BME680_I2C_ADDR_SECONDARY, Wire);
-    iaqSensor.setConfig(bsec_config_iaq);
-    bsec_virtual_sensor_t sensorList[7] = {
-      BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_TEMPERATURE,
-      BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_HUMIDITY,
+    bsec_virtual_sensor_t sensorList[10] = {
+      BSEC_OUTPUT_RAW_TEMPERATURE,
       BSEC_OUTPUT_RAW_PRESSURE,
+      BSEC_OUTPUT_RAW_HUMIDITY,
       BSEC_OUTPUT_RAW_GAS,
+      BSEC_OUTPUT_IAQ,
       BSEC_OUTPUT_STATIC_IAQ,
       BSEC_OUTPUT_CO2_EQUIVALENT,
       BSEC_OUTPUT_BREATH_VOC_EQUIVALENT,
+      BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_TEMPERATURE,
+      BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_HUMIDITY,
     };
-    iaqSensor.updateSubscription(sensorList, 7, BSEC_SAMPLE_RATE_LP);
+    iaqSensor.updateSubscription(sensorList, 10, BSEC_SAMPLE_RATE_LP);
   }
 
   void update() override {
