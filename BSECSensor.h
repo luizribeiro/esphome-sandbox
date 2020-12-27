d728447cc52392071227848f185b0a7f8479a425
Luiz Ribeiro <luizribeiro@gmail.com>
2020-12-27 16:38:04 -0600

    Bunch of changes ¯\_(ツ)_/¯

--- BSECSensor.h
+++ BSECSensor.h
@@ -1,6 +1,10 @@
 #include "esphome.h"
 #include "bsec.h"
 
+const uint8_t bsec_config_iaq[] = {
+    #include "config/generic_33v_3s_28d/bsec_iaq.txt"
+};
+
 class BSECSensor : public PollingComponent, public Sensor {
  private:
   Bsec bsec;
@@ -12,10 +16,11 @@ class BSECSensor : public PollingComponent, public Sensor {
   Sensor *airQualitySensor = new Sensor();
   Sensor *gasResistanceSensor = new Sensor();
 
-  BSECSensor() : PollingComponent(30000) { }
+  BSECSensor() : PollingComponent(300000) { }
 
   void setup() override {
     bsec.begin(BME680_I2C_ADDR_SECONDARY, Wire);
+    bsec.setConfig(bsec_config_iaq);
     bsec_virtual_sensor_t sensorList[] = {
       BSEC_OUTPUT_RAW_TEMPERATURE,
       BSEC_OUTPUT_RAW_PRESSURE,
@@ -37,6 +42,26 @@ class BSECSensor : public PollingComponent, public Sensor {
 
   void update() override {
     bsec.run();
+
+    if (bsec.status != BSEC_OK) {
+      if (bsec.status < BSEC_OK) {
+        ESP_LOGE("bsec", "BSEC error code: %d", bsec.status);
+        status_set_error();
+        return;
+      } else {
+        ESP_LOGW("bsec", "BSEC warning code: %d", bsec.status);
+        status_set_warning();
+      }
+    }
+
+    if (bsec.bme680Status < BME680_OK) {
+      ESP_LOGE("bsec", "BSEC error code: %d", bsec.bme680Status);
+      status_set_error();
+      return;
+    } else {
+      ESP_LOGW("bsec", "BSEC warning code: %d", bsec.bme680Status);
+      status_set_warning();
+    }
 
     temperatureSensor->publish_state(bsec.temperature);
     pressureSensor->publish_state(bsec.pressure);
